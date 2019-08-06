9fb583111a9ec9bbb1d80c6bd2bbecf256db9666
Tim Schartman <tschartman@yahoo.com>
2019-08-05 23:01:23 -0500

    added find13 function which finds the only combo that is the same for 13 words

--- daily-programmer/smooshed-morse-code/src.py
+++ daily-programmer/smooshed-morse-code/src.py
@@ -1,4 +1,5 @@
 
+
 def smorse(word):
     morse = [ ".- ", "-... ", "-.-. ", "-.. ", ". ", "..-. ", "--. ", ".... ", ".. ",".--- ", "-.- ", ".-.. ", "-- ", "-. ", "--- ", ".--. ", "--.- ",".-. ", "... ", "- ", "..- ","...- ", ".-- ", "-..- ", "-.-- ", "--.. ", "|" ]
     alpha = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u","v", "w", "x", "y", "z", " " ]
@@ -6,4 +7,19 @@ def smorse(word):
     decoder = dict(zip(alpha, morse))
     return ''.join(map(lambda y : decoder[y].strip(' '), word))
 
+def find13():
+    freqMap = {}
+    with open('./enable1.txt') as file:
+        for line in file:
+            word = smorse(line.strip('\n'))
+            if( word in freqMap):
+                freqMap[word] += 1
+            else:
+                freqMap[word] = 1
+
+    for key, value in freqMap.items():
+        if (value == 13):
+            return key
+
 print(smorse(input("Enter text to be decoded: ")))
+print(find13())
